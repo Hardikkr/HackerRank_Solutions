d61ae25622237008d7d7d852763b17b52a0310c6
Hardik kumar <61787056+Hardikkr@users.noreply.github.com>
2020-07-24 17:28:24 +0530

    rating

---
+++ Problem Solving/Compare the Triplets.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[100],b[100];
+    int i,alice=0,bob=0;
+
+    for(i=0; i<3; i++){
+        cin>>a[i];
+    }
+    for(i=0; i<3; i++){
+        cin>>b[i];
+    }
+
+    for(i=0; i<3; i++){
+        if(a[i]>b[i])
+          alice++;
+        else if(b[i]>a[i])
+          bob++;    
+    }
+
+    cout<<alice<<" "<<bob;
+
+    return 0;
+}
