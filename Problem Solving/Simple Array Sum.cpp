af166b49209015a7cc7357c4c67d8793e809631d
Hardik kumar <61787056+Hardikkr@users.noreply.github.com>
2020-07-24 17:25:40 +0530

    array sum

---
+++ Problem Solving/Simple Array Sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i, n;
+    cin>>n;
+
+    int arr[n];
+    for(i=0; i<n; i++){
+      cin>>arr[i];
+    }
+
+    int sum = 0;
+    for(i=0; i<n; i++){
+      sum = sum + arr[i];
+    }
+
+    cout<<sum;
+
+    return 0;
+}
